b13429fa248ca62f5b65607900eb9e0e7e336be3
AmielRe <amieli1999@gmail.com>
2023-01-03 09:06:00 +0200

    Create server with products route.

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+const http = require('http').Server(app);
+require('dotenv/config');
+
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,            // access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }))
+
+// Routers
+const productsRoute = require('./routes/products');
+
+app.use('/products', productsRoute)
+
+mongoose.connect(`mongodb${process.env.prod}://${process.env.dbUser}:${process.env.dbPass}@${process.env.dbHost}`)
+http.listen(process.env.PORT || 5000);
