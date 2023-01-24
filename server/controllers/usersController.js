c17e774c15d64b10442d98ac038e8dd808c85853
AmielRe <amieli1999@gmail.com>
2023-01-24 11:40:00 +0200

    Create users route and controller.

---
+++ server/controllers/usersController.js
@@ -0,0 +1,81 @@
+const User = require('../models/user');
+const Config = require('../config/roles');
+
+// Get all users
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    }
+    catch (err) {
+        res.status(500).json({ "error": err });
+    }
+}
+
+const getUserByEmail = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(500).json({ "error": err });
+    }
+}
+
+const createNewUser = async (req, res) => {
+    const user = new User({
+        email: req.body.email,
+        role: Config.ROLES.guest
+    });
+
+    console.log(user)
+
+    try {
+        await user.save();
+
+        res.status(200).json({ "status": "New user was added" });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ "status": "Failed to create new user" });
+    }
+}
+
+const updateUser = async (req, res) => {
+    var newData = {
+        "role": req.body["0"]
+    }
+
+    try {
+        User.updateOne({ 'email': req.params.email }, { $set: newData }, function (err, response) {
+            if (err) {
+                res.status(500).json({ "error": Response.user.queryError })
+            }
+            else {
+                res.status(200).json({ "status": "User has been updated" })
+            }
+        });
+    }
+    catch (err) {
+        res.status(500).json({ "error": Response.user.queryError })
+    }
+}
+
+// Will delete a user from the system - Admin only
+const deleteUser = async (req, res) => {
+    try {
+        await User.deleteOne({ "_id": req.params.id });
+
+        res.status(200).json({ "status": "User deleted!" });
+    }
+    catch (err) {
+        res.status(500).json({ "error": Response.user.deleteError })
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserByEmail,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
