f92289d615a35fb4f11eb87c40e1577b8ccb6b14
AmielRe <amieli1999@gmail.com>
2023-01-27 09:20:07 +0200

    Organize all cards to be same size.
    Remove unused css.

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -42,7 +42,7 @@ class PostList extends Component {
 
         return (
             <div className='container'>
-                <Row md={1} lg={2} xl={3} xxl={4} className="g-4">
+                <Row md={1} lg={2} xl={3} xxl={4} className="g-1">
                     {
                         posts.length ?
                             posts.map(post =>
