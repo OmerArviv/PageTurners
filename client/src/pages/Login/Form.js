5f5caf57fe120820a34ab94bf9276348c975cb37
AmielRe <amieli1999@gmail.com>
2023-01-31 23:13:31 +0200

    Redesign login page.

--- client/src/pages/Login/Form.js
+++ client/src/pages/Login/Form.js
@@ -1,15 +1,19 @@
 import './Form.css';
+import { useRef } from 'react';
 
-function Form({ option }) {
+function Form({ option, onClickResult }) {
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
     return (
-        <form className='account-form' onSubmit={(evt) => evt.preventDefault()}>
-            <div className={'account-form-fields ' + (option === 1 ? 'sign-in' : (option === 2 ? 'sign-up' : 'forgot'))}>
-                <input id='email' name='email' type='email' placeholder='E-mail' required />
-                <input id='password' name='password' type='password' placeholder='Password' required={option === 1 || option === 2 ? true : false} disabled={option === 3 ? true : false} />
-                <input id='repeat-password' name='repeat-password' type='password' placeholder='Repeat password' required={option === 2 ? true : false} disabled={option === 1 || option === 3 ? true : false} />
+        <form className='account-form' onSubmit={(evt) => onClickResult(evt, emailInputRef, passwordInputRef)}>
+            <div className={'account-form-fields ' + (option === 1 ? 'sign-in' : 'sign-up')}>
+                <input id='email' name='email' type='email' placeholder='E-mail' required ref={emailInputRef} />
+                <input id='password' name='password' type='password' placeholder='Password' ref={passwordInputRef} required={option === 1 || option === 2 ? true : false} />
+                <input id='repeat-password' name='repeat-password' type='password' placeholder='Repeat password' required={option === 2 ? true : false} disabled={option === 1 ? true : false} />
             </div>
             <button className='btn-submit-form' type='submit'>
-                {option === 1 ? 'Sign in' : (option === 2 ? 'Sign up' : 'Reset password')}
+                {option === 1 ? 'Sign in' : 'Sign up'}
             </button>
         </form>
     )
