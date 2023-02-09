d89bc44adac8d1f8d3a675bda30f4499b60e2920
AmielRe <amieli1999@gmail.com>
2023-02-09 13:59:21 +0200

    Fix warnings.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -24,7 +24,7 @@ function About(props) {
                                 </span>
                             </div>
                             <h4 className="fw-light">
-                                <a>Love books</a>
+                                <button className="about-min-title">Love books</button>
                             </h4>
                             <p className="text-black-50 f-15 line-h-26">
                                 PageTurners is a book store that has been providing quality reading materials to book lovers for many years. Our store is dedicated to fostering a love of reading in all of our customers. Whether you are a seasoned reader or just starting to explore the world of books, we have something for everyone.
@@ -40,7 +40,7 @@ function About(props) {
                                 </span>
                             </div>
                             <h4 className="fw-light">
-                                <a>Diverse styles</a>
+                                <button className="about-min-title">Diverse styles</button>
                             </h4>
                             <p className="f-15 text-black-50 line-h-26">
                                 Our knowledgeable staff is always on hand to help you find the perfect book for your reading needs. We carefully curate our selection of books to ensure that we have a diverse range of genres and styles for our customers to choose from. From best-selling novels to obscure works of poetry, you are sure to find something you love at PageTurners.
@@ -56,7 +56,7 @@ function About(props) {
                                 </span>
                             </div>
                             <h4 className="fw-light">
-                                <a>Affordable</a>
+                                <button className="about-min-title">Affordable</button>
                             </h4>
                             <p className="f-15 text-black-50 line-h-26">
                                 At PageTurners, we believe that reading should be accessible and affordable for everyone. That's why we offer competitive prices on all of our books and regularly host events and promotions to help you get the most out of your reading experience.
