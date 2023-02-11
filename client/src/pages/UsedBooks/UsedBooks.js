811f4dc08c1a27cbf7a68325c04bd63b69617e3e
Omer <omerarviv801@gmail.com>
2023-02-11 20:32:41 +0200

    added custom hook in usedBooks2

--- client/src/pages/UsedBooks/UsedBooks.js
+++ client/src/pages/UsedBooks/UsedBooks.js
@@ -8,7 +8,8 @@ export default function UsedBooks() {
             <div className="usedBooks-container">
                 <h1 className="usedBooks-header">Do you have any used books that you don't need ?</h1>
                 <p className="usedBooks-text">
-                    Donating used books benefits the community by supporting literacy and reducing waste. Your generosity can make a positive impact by providing access to books for those in need and spreading the joy of reading. Consider donating to a local library, school, or charity.                </p>
+                    Donating used books benefits the community by supporting literacy and reducing waste. Your generosity can make a positive impact by providing access to books for those in need and spreading the joy of reading. Consider donating to a local library, school, or charity.
+                </p>
                 <Counter />
             </div>
         </div>
