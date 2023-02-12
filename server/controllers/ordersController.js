3b2ebca3556312216632a0ff5d541f6902bcdede
AmielRe <amieli1999@gmail.com>
2023-02-12 11:33:51 +0200

    Create method to return the required amount of profitable books.
    This query get the limit amount of profitable books, aggregates over all orders and returns the most profitable books.

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -10,6 +10,25 @@ const getAllOrders = async (req, res) => {
     }
 };
 
+const getMostProfitableBooks = async (req, res) => {
+    try {
+        const orders = await Order.aggregate([
+            { $unwind: "$books" },
+            { $lookup: { "from": "Book", "localField": "books.prod", "foreignField": "_id", "as": "book" } },
+            { $group: { _id: { $first: "$book._id" }, book: { $first: "$book" }, total: { $sum: { $multiply: [{ $first: "$book.price" }, "$books.qty"] } } } },
+            { $sort: { total: -1 } },
+            { $limit: parseInt(req.params.limit) }
+        ])
+
+        res.status(200).json(orders);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ "error": err });
+    }
+}
+
 module.exports = {
-    getAllOrders
+    getAllOrders,
+    getMostProfitableBooks
 };
